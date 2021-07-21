8b5ab936905a1d6a420d98b110f8d513133ea547
Renato Aquino <repitriano12@gmail.com>
2021-07-21 11:21:03 -0300

    update: created the client application

---
+++ go-grpc/client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(String[]args){
+            using (HttpClient client = new HttpClient()){
+                var json = System.Text.Json.JsonSerializer.Serialize(new {
+                    name = "Renato"
+                });
+
+                Console.WriteLine($"request data {json}");
+
+                var content = new StringContent(json);
+
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                var resp = await client.PostAsync("http://localhost:8090/v1/example/echo",content);
+
+                var res = await resp.Content.ReadAsStringAsync();
+
+                Console.WriteLine($"response result {res}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
